201928ae6e646712ca1d31d43a53f84cc6c54bd0
Rushi Gandhi <rushigandhi@huntsman-ve701-0398.eduroam.wlan.private.upenn.edu>
2018-09-08 00:44:05 -0400

    Complete Join Room Page

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Player from './pages/player/player';
+import Landing from './pages/landing/landing';
+import Join from './pages/join/join';
+import Host from './pages/host/host';
+
+
+
 import './index.css';
 
 class App extends React.Component { 
     render() {
         return (
             <div>
-                <Player />
+                <Join />
 
             </div>
         )
---
+++ client/src/pages/host/host.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './host.css';
+
+class Host extends Component {
+    render() {
+        return (
+            <div className='container'>
+            <div class="flex-container">
+                <div class="row"> 
+                    <div class="flex-item">
+                        <form>
+                            <input type="text" name="name" class="question" id="nme" required autocomplete="off" />
+                            <label for="nme"><span>Enter the room code</span></label>
+                            <textarea name="message" rows="2" class="question" id="msg" required autocomplete="off"></textarea>
+                        </form>
+                    </div>
+                </div>
+            </div>
+                <div className='tabBar'>
+                    <div className='button' style={{backgroundColor:'#EF4A30', fontWeight: '700'}}>
+                        JOIN ROOM
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Host;
