e5221de5d2092d01d65c611867f1936d5fb5b8cf
mufeez-amjad <zeefumdajma@outlook.com>
2018-09-09 01:31:37 -0400

    added animated fire

--- client/src/components/queueObject/queueObject.js
+++ client/src/components/queueObject/queueObject.js
@@ -1,31 +1,53 @@
 import React, { Component } from "react";
-import button from "react";
 import "./queueObject.css";
-
-import album from "../mediaControl/album.jpg";
-import yellowFlame from "./heat1.png";
-import orangeFlame from "./heat2.png";
-import redFlame from "./heat3.png";
+import redFire from './redfire.gif';
+import redPinkFire from './redpinkfire.gif';
+import pinkFire from './pinkfire.gif';
+import magentaFire from './magentafire.gif';
+import darkMagentaFire from './darkmagentafire.gif';
+import purpleFire from './purplefire.gif';
 
 class QueueObject extends Component {
   constructor(props) {
     super();
   }
 
+  state = {
+    query: "",
+    searchResults: [],
+    color: "#"
+  };
+
+  flame = () => { 
+    if (this.props.upvoteCount < 6) {
+      return 'redfire';
+    }
+  }
+
   render() {
     const { thumbnail, title, artist, upvoteCount, makeLit, URL } = this.props;
+    var color = ""
+    if (this.props.upvoteCount < 6){
+      color = "#EF4A30"
+    }
+    else if (this.props.upvoteCount < 11){
+      color = "#f40d30"
+    }
+
     return (
       <div className="song">
         <img src={thumbnail} className="album" />
 
         <div className="metadata">
+        {/* this.props.title.slice(0,20) */}
           <h1>{title}</h1>
           <h2>{artist}</h2>
         </div>
 
         <button className="heat" onClick={() => makeLit(URL)}>
-          <img src={yellowFlame} />
-          <h1>{upvoteCount}</h1>
+          <h1 style={{backgroundColor: this.state.backgroundColor}}>{upvoteCount}</h1>
+          <img src={upvoteCount < 6 ? require("./redfire.gif") : upvoteCount < 11 ? require("./redpinkfire.gif") : upvoteCount < 16 ? require("./pinkfire.gif") : upvoteCount < 21 ? require("./magentafire.gif") : 
+            upvoteCount < 26 ? require("./darkmagentafire.gif") : upvoteCount < 31 ? require("./purplefire.gif") : require("./purplefire.gif")} />
         </button>
       </div>
     );
